f70863aee2c4ececcdd39fbdfeed8f82af4a2b57
chuan-hao, you <howtomakeaturn@hotmail.com>
2017-07-18 15:32:58 +0800

    design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,86 +10,80 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+        <link href="/css/bulma.min.css" rel="stylesheet" type="text/css">
+
+
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+            .box .tag{
+                margin-bottom: 5px;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .button.is-block {
+                display: block;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <section class="hero is-primary">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Case X
+              </h1>
+              <h2 class="subtitle">
+                由爬蟲機器人到處蒐集案件資訊的資料蒐集站。
+              </h2>
             </div>
-        </div>
+          </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                @foreach($rows as $row)
+                <h3 class="title is-3 has-text-centered">{{$row['category']}}</h3>
+                @for($i=0; $i<2; $i++)
+                <div class="box">
+                    <div class="columns">
+                        <div class="column">
+                            <h5 class="title is-5">
+                            {{$row['title']}}
+                            </h5>
+                            <div>
+                                <span class="tag is-black is-medium">工程</span>
+                                <span class="tag is-black is-medium">論文摘要</span>
+                                <span class="tag is-black is-medium">工程</span>
+                                <span class="tag is-black is-medium">工程</span>
+                                <span class="tag is-black is-medium">工程</span>
+                            </div>
+                            <div class="content is-medium" style="margin-top: 16px;">
+                                <pre>@include('_' . ($i +1))</pre>
+                            </div>
+                        </div>
+                        <div class="column is-1 is-hidden-mobile">
+                            31 分鐘前
+                        </div>
+                        <div class="column is-2">
+                              <a class="button is-primary is-medium is-block">我要接案</a>
+                        </div>
+                    </div>
+                </div>
+                @endfor
+                <br>
+                @endforeach
+            </div>
+        </section>
+
+        <footer class="footer">
+          <div class="container">
+            <div class="content has-text-centered">
+              <p>
+                <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+              </p>
+            </div>
+          </div>
+        </footer>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $rows = [
+        [
+            'title' => 'HTML 與CSS 修改',
+            'category' => '程式',
+            'description' => '修改 ghost blog theme, ghost blog 使用handlebar模板引擎，不懂的話我可以教你，所以只要幫忙將原本相關的HTML與CSS修改好即可'
+        ],
+        [
+            'title' => '2/字_筆_中譯英_摘要_666字_20170717',
+            'category' => '翻譯',
+            'description' => '[必]所屬領域：工程 [必]文件類型：論文摘要'
+        ],
+        [
+            'title' => '菜單設計',
+            'category' => '設計',
+            'description' => '菜單設計排版 插圖 （拍照）'
+        ],
+    ];
+
+    return view('welcome', compact('rows'));
 });
